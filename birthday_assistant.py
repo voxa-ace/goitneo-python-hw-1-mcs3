b8fe0b831db4a8672c57171dc99b5fbc239effd5
Volodymyr Kashnikov <vkashnikov19@gmail.com>
2023-10-13 21:02:41 +0300

    developed birthday assistant mvp

---
+++ birthday_assistant.py
@@ -0,0 +1,33 @@
+import users
+import datetime
+
+
+def get_birthdays_per_week(users):
+    current_date = datetime.datetime.today().date()
+    users_to_present: dict[str, list] = {}
+
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()  
+        birthday_this_year = birthday.replace(year=current_date.year)
+        
+        if birthday_this_year < current_date:
+            birthday_this_year = birthday.replace(year=current_date.year + 1)
+            
+        delta_days = (birthday_this_year - current_date).days
+        
+        if delta_days < 7:
+            day_name = birthday_this_year.strftime('%A')
+            if day_name not in users_to_present:
+                users_to_present[day_name] = []
+            users_to_present[day_name].append(name)
+
+    for day, users_list in users_to_present.items():
+        print(f"{day}: {', '.join(users_list)}")
+
+# отсортировать дни
+
+
+
+if __name__ == "__main__":
+    get_birthdays_per_week(users.users)
---
+++ users.py
@@ -0,0 +1,105 @@
+import datetime
+
+
+users = [
+    {
+        "name": "Billy Bush",
+        "birthday": datetime.datetime(1971, 10, 13),
+    },
+    {
+        "name": "Bill Gates",
+        "birthday": datetime.datetime(1955, 10, 28),
+    },
+    {
+        "name": "Steve Jobs",
+        "birthday": datetime.datetime(1955, 2, 24),
+    },
+    {
+        "name": "Oprah Winfrey",
+        "birthday": datetime.datetime(1954, 1, 29),
+    },
+    {
+        "name": "Elon Musk",
+        "birthday": datetime.datetime(1971, 6, 28),
+    },
+    {
+        "name": "Taylor Swift",
+        "birthday": datetime.datetime(1989, 12, 13),
+    },
+    {
+        "name": "Beyoncé",
+        "birthday": datetime.datetime(1981, 9, 4),
+    },
+    {
+        "name": "Justin Bieber",
+        "birthday": datetime.datetime(1994, 3, 1),
+    },
+    {
+        "name": "Selena Gomez",
+        "birthday": datetime.datetime(1992, 7, 22),
+    },
+    {
+        "name": "Keanu Reeves",
+        "birthday": datetime.datetime(1964, 9, 2),
+    },
+    {
+        "name": "Jennifer Lawrence",
+        "birthday": datetime.datetime(1990, 8, 15),
+    },
+    {
+        "name": "Emma Watson",
+        "birthday": datetime.datetime(1990, 4, 15),
+    },
+    {
+        "name": "Tom Hanks",
+        "birthday": datetime.datetime(1956, 7, 9),
+    },
+    {
+        "name": "Julia Roberts",
+        "birthday": datetime.datetime(1967, 10, 28),
+    },
+    {
+        "name": "Brad Pitt",
+        "birthday": datetime.datetime(1963, 12, 18),
+    },
+    {
+        "name": "Angelina Jolie",
+        "birthday": datetime.datetime(1975, 6, 4),
+    },
+    {
+        "name": "Dwayne Johnson",
+        "birthday": datetime.datetime(1972, 5, 2),
+    },
+    {
+        "name": "LeBron James",
+        "birthday": datetime.datetime(1984, 12, 30),
+    },
+    {
+        "name": "Michael Jordan",
+        "birthday": datetime.datetime(1963, 2, 17),
+    },
+    {
+        "name": "Lionel Messi",
+        "birthday": datetime.datetime(1987, 6, 24),
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "birthday": datetime.datetime(1985, 2, 5),
+    },
+    {
+        "name": "Serena Williams",
+        "birthday": datetime.datetime(1981, 9, 26),
+    },
+    {
+        "name": "Roger Federer",
+        "birthday": datetime.datetime(1981, 8, 8),
+    },
+    {
+        "name": "Michael Phelps",
+        "birthday": datetime.datetime(1985, 8, 30),
+    },
+    {
+        "name": "Usain Bolt",
+        "birthday": datetime.datetime(1986, 8, 21),
+    },
+]
